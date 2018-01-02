790a95df8e0b3ba91ede134e1a5f5053b6476535
fmgit <mendesmateusfelipe@gmail.com>
2018-01-02 12:38:09 -0200

    Delete atividade12.py

--- atividade12.py
+++
@@ -1,9 +0,0 @@
-'''Construa um código recursivo de contagem regressiva'''
-def contagem_regressiva(n):
-   while n>0:
-      print (n)
-      n = n-1
-   print ("Fogo!")
- 
-contagem_regressiva(10)
-  
